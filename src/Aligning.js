9a0a28e3e3e7cb9db9b5ecaa6058d28a0c58c566
Oktora Kevin Arigi <oktora.kevinarigi@ai.astra.co.id>
2020-03-12 21:12:46 +0700

    Aligning

---
+++ src/Aligning.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Aligning.css';
+
+function Aligning() {
+  return (
+    <div id="content">
+        <div class="one">1 with content</div>
+		<div class="two">2</div>
+		<div class="three">3</div>
+		<div class="four">4</div>
+		<div class="five">5</div>
+		<div class="six">6</div>
+    </div>
+  );
+}
+
+export default Aligning;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import App from './App';
 // import GridLine from './GridLine'
-import NestedGrids from './NestedGrids'
+// import NestedGrids from './NestedGrids'
+import Aligning from './Aligning'
+
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<NestedGrids />, document.getElementById('root'));
+ReactDOM.render(<Aligning />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
