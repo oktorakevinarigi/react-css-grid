ef63e074a24710839a852615dcfca4b92431fabb
Oktora Kevin Arigi <oktora.kevinarigi@ai.astra.co.id>
2020-03-14 15:30:28 +0700

    Grid Area dan Responsive

---
+++ src/GridAreas.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './GridAreas.css';
+
+function GridAreas() {
+  return (
+      <>
+        <div id="content">
+            <header>Header</header>
+            <main>Main</main>
+            <section>Section</section>
+            <aside>Aside</aside>
+            <nav>Nav</nav>
+            <footer>Footer</footer>
+        </div>
+    </>
+  );
+}
+
+export default GridAreas;
--- src/index.js
+++ src/index.js
@@ -5,11 +5,12 @@ import ReactDOM from 'react-dom';
 // import NestedGrids from './NestedGrids'
 // import Aligning from './Aligning'
 // import ColumnGrid12 from './ColumnGrid12'
-import Mosaic from './Mosaic'
+// import Mosaic from './Mosaic'
+import GridAreas from './GridAreas'
 
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Mosaic />, document.getElementById('root'));
+ReactDOM.render(<GridAreas />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
